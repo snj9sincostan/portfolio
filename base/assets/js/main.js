0ea07e7fb5df06d7774d288bc333e17ab7cc3ac7
shinji <shinwest9276@gmail.com>
2021-07-03 00:08:27 +0900

    update codeのアップデート

--- base/assets/js/main.js
+++ base/assets/js/main.js
@@ -116,4 +116,23 @@ jQuery(document).ready(function () {
   }
   init();
 
+  function a() {
+    $('.btn-6')
+      .on('mouseenter', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      });
+    $('.btn-6').click(function(){return false});
+    // $('[href=#]').click(function(){return false});
+  }
+  a()
+
 });
