8bfb90f447e01bc2187cbe5dc58706d94fba652b
shinji <shinwest9276@gmail.com>
2021-07-06 16:59:21 +0900

    [update] codeのアップデート

--- wp-content/themes/my-portfolio/portfolio.php
+++ wp-content/themes/my-portfolio/portfolio.php
@@ -10,7 +10,7 @@ Template Name: portfolio
     <h2 class="c-heading">Portfolio</h2>
     <div class="p-page-portfolio__wrapper">
       <p class="p-page-portfolio__image">
-        <img src="/assets/images/Hero.png" alt="ポートフォリオ画像">
+        <img src="<?php echo get_template_directory_uri();?>/assets/images/Hero.png" alt="ポートフォリオ画像">
       </p>
       <div class="p-page-portfolio__description">
         <h3 class="c-secondaryHeading" data-en="What">ポートフォリオの概要</h3>
@@ -44,7 +44,7 @@ Template Name: portfolio
       </div>
     </div>
     <button class="c-primarybutton p-page-portfolio__button">
-      <a href="index.html">トップへ戻る</a>
+      <a href="<?php echo home_url(); ?>">トップへ戻る</a>
     </button>
   </div>
 </main>
