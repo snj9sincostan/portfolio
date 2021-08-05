586336ac06b2e10b3c8f0f95f7291fb0365af3c7
shinji <shinwest9276@gmail.com>
2021-08-05 16:20:39 +0900

    [fix] クォーテーションの統一

--- wp-content/themes/my-portfolio/assets/js/main.js
+++ wp-content/themes/my-portfolio/assets/js/main.js
@@ -2,30 +2,30 @@ jQuery(document).ready(() => {
 
   const smoothScroll = ()=> {
     const speed = 700;
-    const headerHeight = $('.l-header').outerHeight();
-    const $headerLink = $('a[href*="#"]');
+    const headerHeight = $(".l-header").outerHeight();
+    const $headerLink = $(`a[href*="#"]`);
 
     const init = (event) => {
       const $this = $(event.currentTarget);
       let href= $this.attr("href");
       const developmentUrl = `${location.protocol}//${location.hostname}:8888${location.pathname}`;
       const productionUrl = `${location.protocol}//${location.hostname}${location.pathname}`;
-      const url = (location.href === 'localhost:8888') ? productionUrl : developmentUrl;
-      href = href.replace(url, '');
-      const target = $(href == "#" || href == "" ? 'html' : href);
+      const url = (location.href === "localhost:8888") ? productionUrl : developmentUrl;
+      href = href.replace(url, "");
+      const target = $(href == "#" || href == "" ? "html" : href);
       const position = target.offset().top - headerHeight;
       $("html, body").animate({scrollTop:position }, speed, "swing");
     }
 
-    $headerLink.on('click', init);
+    $headerLink.on("click", init);
   }
 
   const addHeaderStyle = () => {
     const $window = $(window);
-    const $header = $('.l-header');
-    const $firstView = $('#firstview');
+    const $header = $(".l-header");
+    const $firstView = $("#firstview");
     const $firstViewHeight = $firstView.outerHeight();
-    const fixed = 'is-fixed';
+    const fixed = "is-fixed";
     const headerHeight = 70;
 
     const init = () => {
@@ -37,59 +37,59 @@ jQuery(document).ready(() => {
       }
     }
 
-    $window.on('scroll', init);
+    $window.on("scroll", init);
   }
 
   const changeBurgerMenu = () => {
-    const $body = $('body');
-    const $header = $('.l-header');
-    const $burgerIcon = $('.l-header__burger');
-    const $headerNav = $('.l-header__nav');
-    const $navItem = $('.l-header__item');
+    const $body = $("body");
+    const $header = $(".l-header");
+    const $burgerIcon = $(".l-header__burger");
+    const $headerNav = $(".l-header__nav");
+    const $navItem = $(".l-header__item");
 
     const clickCloseNavMenu = () => {
-      $burgerIcon.trigger('click');
+      $burgerIcon.trigger("click");
     }
 
     const init = () => {
-      $body.toggleClass('u-no-scroll');
-      $burgerIcon.toggleClass('is-open');
-      $headerNav.toggleClass('is-show');
-      $header.toggleClass('is-active');
-      $navItem.on('click', clickCloseNavMenu);
+      $body.toggleClass("u-no-scroll");
+      $burgerIcon.toggleClass("is-open");
+      $headerNav.toggleClass("is-show");
+      $header.toggleClass("is-active");
+      $navItem.on("click", clickCloseNavMenu);
     }
 
-    $burgerIcon.on('click', init);
+    $burgerIcon.on("click", init);
   }
 
   const changeSkillPanel = () => {
-    const $tabDataFilter = $('#tab [data-filter]');
-    const $skillIconCategory = $('#icons [data-category]');
+    const $tabDataFilter = $("#tab [data-filter]");
+    const $skillIconCategory = $("#icons [data-category]");
 
     const init = (event) => {
       event.preventDefault();
       const $this = $(event.currentTarget);
-      $tabDataFilter.removeClass('is-active');
-      $this.addClass('is-active');
-      const $activeTabHaveDataFilter = $this.attr('data-filter');
-      if ($activeTabHaveDataFilter == 'all') {
-        $skillIconCategory.removeClass('is-animated').fadeOut().promise().done(() => {
-          $skillIconCategory.addClass('is-animated').fadeIn();
+      $tabDataFilter.removeClass("is-active");
+      $this.addClass("is-active");
+      const $activeTabHaveDataFilter = $this.attr("data-filter");
+      if ($activeTabHaveDataFilter == "all") {
+        $skillIconCategory.removeClass("is-animated").fadeOut().promise().done(() => {
+          $skillIconCategory.addClass("is-animated").fadeIn();
         });
       } else {
-        $skillIconCategory.removeClass('is-animated').fadeOut().promise().done(() => {
-          $skillIconCategory.filter(`[data-category = "${$activeTabHaveDataFilter}"]`).addClass('is-animated').fadeIn();
+        $skillIconCategory.removeClass("is-animated").fadeOut().promise().done(() => {
+          $skillIconCategory.filter(`[data-category = "${$activeTabHaveDataFilter}"]`).addClass("is-animated").fadeIn();
         });
       }
     }
 
-    $tabDataFilter.on('click', init);
+    $tabDataFilter.on("click", init);
   }
 
   const showAnimation = () => {
     const $window = $(window);
-    const $animationAttributeHaveElements = $('[animation]');
-    $animationAttributeHaveElements.addClass('u-invisible');
+    const $animationAttributeHaveElements = $("[animation]");
+    $animationAttributeHaveElements.addClass("u-invisible");
 
     const addAnimationStyle = () => {
       $animationAttributeHaveElements.each((index, element) => {
@@ -98,14 +98,14 @@ jQuery(document).ready(() => {
         const elementPosition = $this .offset().top;
         const scroll = $window.scrollTop();
         const position = elementPosition - (window.innerHeight * 2) / 5;
-        if ($element.hasAttribute('animation') && (scroll > position)) {
-          const animation = $element.getAttribute('animation');
-          $this.removeClass('u-invisible').addClass(animation);
+        if ($element.hasAttribute("animation") && (scroll > position)) {
+          const animation = $element.getAttribute("animation");
+          $this.removeClass("u-invisible").addClass(animation);
         }
       });
     }
 
-    $window.on('scroll', addAnimationStyle);
+    $window.on("scroll", addAnimationStyle);
   }
 
   const init = () => {
